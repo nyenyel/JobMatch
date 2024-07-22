ec5adaa44b8d3d4dbcc2345b101de66e97a88f1d
nyenyel <154278884+nyenyel@users.noreply.github.com>
2024-07-22 09:18:06 +0800

    user and reviews model

--- Backend/app/Models/Review.php
+++ Backend/app/Models/Review.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Review extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+    protected $fillable = [
+        'rate',
+        'review',
+        'reviewed_by',
+        'reviewed_for',
+    ];
+
+    public function reviewedBy():BelongsTo{
+        return $this->belongsTo(User::class, 'reviewed_by');
+    }
+    public function reviewedFor():BelongsTo{
+        return $this->belongsTo(User::class, 'reviewed_for');
+    }
 }
