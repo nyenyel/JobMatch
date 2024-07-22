547ba7cc1aebfb80f054353f46833794b1ec30b4
nyenyel <154278884+nyenyel@users.noreply.github.com>
2024-07-22 09:04:20 +0800

    applicant model

--- Backend/app/Models/Applicant/ApplicantExperience.php
+++ Backend/app/Models/Applicant/ApplicantExperience.php
@@ -2,10 +2,21 @@
 
 namespace App\Models\Applicant;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ApplicantExperience extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+    protected $fillable = [
+        'title',
+        'desc',
+        'applicant_id',
+    ];
+    public function applicant():BelongsTo{
+        return $this->belongsTo(User::class, 'applicant_id');
+    }
 }
