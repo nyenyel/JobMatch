8683563138d5dd25454b0d4bf9ae40e5c7689f45
nyenyel <154278884+nyenyel@users.noreply.github.com>
2024-07-23 21:37:12 +0800

    Library profession crud and library request and rosources

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\API\v1\BasicController\Library\LibProfessionController;
+use App\Models\Library\LibProfession;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::prefix('v1')->group( function (){
+    Route::prefix('basic-controller')->group(function (){
+        Route::apiResource('profession', LibProfessionController::class);
+    });
+});
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
