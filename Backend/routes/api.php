5af25ed5e27eab5640fc960133f82d8840ac0784
nyenyel <154278884+nyenyel@users.noreply.github.com>
2024-07-23 22:30:23 +0800

    Skill type

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\v1\BasicController\Library\LibProfessionController;
+use App\Http\Controllers\API\v1\BasicController\Library\LibSkillTypeController;
 use App\Models\Library\LibProfession;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('v1')->group( function (){
     Route::prefix('basic-controller')->group(function (){
         Route::apiResource('profession', LibProfessionController::class);
+        Route::apiResource('skill-type', LibSkillTypeController::class);
     });
 });
 Route::get('/user', function (Request $request) {
