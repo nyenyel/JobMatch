00f9138baa26d852e13ad3a37989b6059a7fc3e6
nyenyel <154278884+nyenyel@users.noreply.github.com>
2024-10-29 05:34:13 +0800

    pass

--- database/migrations/2024_07_21_234660_create_users_table.php
+++ database/migrations/2024_07_21_234660_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('email')->unique();
-            $table->string('pasesword');
+            $table->string('password');
             $table->string('username');
             $table->string('phone_no');
             $table->string('first_name');
